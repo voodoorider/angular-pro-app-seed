94ec9754cfd1d836fdb89368793548c7f9b005da
Wojciech Owczarczyk <wojciech.owczarczyk@igt.com>
2017-10-09 11:27:06 +0200

    Reactive workout form

--- src/health/workouts/components/workout-form/workout-form.component.ts
+++ src/health/workouts/components/workout-form/workout-form.component.ts
@@ -8,7 +8,7 @@ import {
   Output,
   SimpleChanges
 } from '@angular/core';
-import { FormArray, FormBuilder, FormControl, Validators } from '@angular/forms'
+import { FormBuilder, Validators } from '@angular/forms'
 
 import { Workout } from '../../../shared/services/workouts/workouts.service';
 
@@ -22,13 +22,44 @@ import { Workout } from '../../../shared/services/workouts/workouts.service';
         <div class="workout-form__name">
           <label>
             <h3>Workout name</h3>
-            <input type="text" placeholder="e.g. Running" formControlName="name">
+            <input type="text" [placeholder]="placeholder" formControlName="name">
             <div class="error" *ngIf="required">Workout name is required</div>
           </label>
           <label>
             <h3>Type</h3>
             <workout-type formControlName="type"></workout-type>
           </label>
+        </div>
+
+        <div class="workout-form__details">
+          <div *ngIf="form.get('type').value === 'strength'">
+            <div class="workout-form__fields" formGroupName="strength">
+              <label>
+                <h3>Reps</h3>
+                <input type="number" formControlName="reps">
+              </label>
+              <label>
+                <h3>Sets</h3>
+                <input type="number" formControlName="sets">
+              </label>
+              <label>
+                <h3>Weight <span>(kg)</span></h3>
+                <input type="number" formControlName="weight">
+              </label>
+            </div>
+          </div>
+          <div *ngIf="form.get('type').value === 'endurance'">
+            <div class="workout-form__fields" formGroupName="endurance">
+              <label>
+                <h3>Distance <span>(km)</span></h3>
+                <input type="number" formControlName="distance">
+              </label>
+              <label>
+                <h3>Duration <span>(minutes)</span></h3>
+                <input type="number" formControlName="duration">
+              </label>
+            </div>
+          </div>
         </div>
 
         <div class="workout-form__submit">
@@ -75,35 +106,36 @@ export class WorkoutFormComponent implements OnInit, OnChanges {
 
   form = this.fb.group({
     name: ['', Validators.required],
-    type: 'strength'
+    type: 'strength',
+    strength: this.fb.group({
+      reps: 0,
+      sets: 0,
+      weight: 0
+    }),
+    endurance: this.fb.group({
+      distance: 0,
+      duration: 0
+    })
   });
 
   constructor(private fb: FormBuilder) {
   }
 
+  get placeholder() {
+    return `e.g. ${this.form.get('type').value === 'strength' ? 'Benchpress' : 'Treadmill'}`;
+  }
+
   ngOnChanges(changes: SimpleChanges) {
-    // if (this.workout && this.workout.name) {
-    //   this.exists = true;
-    //   this.emptyIngredients();
-    //
-    //   const value = this.workout;
-    //   this.form.patchValue(value);
-    //
-    //   if (value.ingredients) {
-    //     for (const item of value.ingredients) {
-    //       this.ingredients.push(new FormControl(item));
-    //     }
-    //   }
-    // }
+    if (this.workout && this.workout.name) {
+      this.exists = true;
+      const value = this.workout;
+      this.form.patchValue(value);
+    }
   }
 
   get required() {
     return this.form.get('name').hasError('required') && this.form.get('name').touched;
   }
-
-  // get ingredients() {
-  //   return this.form.get('ingredients') as FormArray;
-  // }
 
   ngOnInit() {
   }
@@ -124,21 +156,8 @@ export class WorkoutFormComponent implements OnInit, OnChanges {
     this.remove.emit(this.form.value);
   }
 
-  // addIngredient() {
-  //   this.ingredients.push(new FormControl(''));
-  // }
-  //
-  // removeIngredient(index: number) {
-  //   this.ingredients.removeAt(index);
-  // }
-
   toggle() {
     this.toggled = !this.toggled;
   }
 
-  // emptyIngredients() {
-  //   while (this.ingredients.controls.length) {
-  //     this.ingredients.removeAt(0);
-  //   }
-  // }
 }
